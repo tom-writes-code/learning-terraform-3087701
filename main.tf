71b895a4aa25d357da0fc3d8a78db0f8a175852d
Tom Sharp <101675587+tom-writes-code@users.noreply.github.com>
2025-02-27 15:20:24 +0000

    update to new syntax

--- main.tf
+++ main.tf
@@ -62,13 +62,26 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
-  ]
+    ex-https = {
+      port            = 443
+      protocol        = "HTTPS"
+      certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+      forward = {
+        target_group_key = "ex-instance"
+      }
+    }
+  }
 
   tags = {
     Environment = "dev"
